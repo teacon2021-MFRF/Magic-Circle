2d53f30cca2c010bb1be98814d5f457fe6db227e
dbydd <1992003927@qq.com>
2021-05-01 17:25:13 +0800

    add something

--- src/main/java/mfrf/magic_circle/item/GemBase.java
+++ src/main/java/mfrf/magic_circle/item/GemBase.java
@@ -1,4 +1,9 @@
 package mfrf.magic_circle.item;
 
-public class GemBase {
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+public class GemBase extends ItemBase implements ICurioItem {
+    public GemBase(Properties p_i48487_1_, String name) {
+        super(p_i48487_1_, name);
+    }
 }
