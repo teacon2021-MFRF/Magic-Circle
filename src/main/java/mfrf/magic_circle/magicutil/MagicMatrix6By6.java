fa9126183f4c76e292a7c685844dc43cd7ff4acd
dbydd <1992003927@qq.com>
2021-05-01 12:56:53 +0800

    init

--- src/main/java/mfrf/magic_circle/magicutil/MagicMatrix6By6.java
+++ src/main/java/mfrf/magic_circle/magicutil/MagicMatrix6By6.java
@@ -19,11 +19,21 @@ public class MagicMatrix6By6 extends BlockMatrix64F implements INBTSerializable<
 
     @Override
     public CompoundNBT serializeNBT() {
-        return null;
+        CompoundNBT compoundNBT = new CompoundNBT();
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 6; c++) {
+                compoundNBT.putDouble("M" + r + c, get(r, c));
+            }
+        }
+        return compoundNBT;
     }
 
     @Override
     public void deserializeNBT(CompoundNBT compoundNBT) {
-
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 6; c++) {
+                this.set(r, c, compoundNBT.getDouble("M" + r + c));
+            }
+        }
     }
 }
