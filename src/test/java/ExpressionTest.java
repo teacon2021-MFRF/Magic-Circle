0878d3a5e1fd71c1507bd508e3183eac48d24ac9
dbydd <1992003927@qq.com>
2021-07-01 11:57:30 +0800

    fixed circle

--- src/test/java/ExpressionTest.java
+++ src/test/java/ExpressionTest.java
@@ -1,18 +1,19 @@
-import com.googlecode.aviator.AviatorEvaluator;
-import net.minecraft.util.math.vector.Vector3f;
-import org.junit.Test;
-
-import java.util.HashMap;
-
-public class ExpressionTest {
-
-    @Test
-    public void calc() {
-        HashMap<String, Object> env = new HashMap<>();
-        env.put("t", 16.0d);
-
-        float execute = (float) ((double) AviatorEvaluator.execute("math.pow(t,16) + 1", env));
-
-        System.out.println(execute);
-    }
-}
+//import com.googlecode.aviator.AviatorEvaluator;
+//import net.minecraft.util.math.vector.Vector3f;
+//import org.junit.Test;
+//
+//import java.util.HashMap;
+//
+//public class ExpressionTest {
+//
+//    @Test
+//    public void calc() {
+//        HashMap<String, Object> env = new HashMap<>();
+//        env.put("t", 16.0d);
+//
+//        float execute = (float) ((double) AviatorEvaluator.execute("math.pow(t,16) + 1", env));
+//
+//        System.out.println(execute);
+//    }
+//}
+//
