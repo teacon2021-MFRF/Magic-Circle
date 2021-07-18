fa52e7b12de69973d148037d9474dba727efdc24
dbydd <1992003927@qq.com>
2021-07-18 18:49:30 +0800

    still got a lot to do

--- src/main/java/mfrf/magic_circle/registry_lists/GuiContainers.java
+++ src/main/java/mfrf/magic_circle/registry_lists/GuiContainers.java
@@ -7,6 +7,7 @@ import mfrf.magic_circle.gui.research_table.ResearchTableContainer;
 import mfrf.magic_circle.knowledges.PlayerKnowledges;
 import net.minecraft.client.Minecraft;
 import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.RegistryObject;
@@ -16,14 +17,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class GuiContainers {
     public static final DeferredRegister<ContainerType<?>> REGISTER = DeferredRegister.create(ForgeRegistries.CONTAINERS, MagicCircle.MOD_ID);
 
-    //    public static final RegistryObject<ContainerType<ResearchTableContainer>> RESEARCH_TABLE_CONTAINER = REGISTER.register("research_table_container", () -> IForgeContainerType.create((windowId, inv, data) -> new ResearchTableContainer(windowId, inv, data.readBlockPos(), DistExecutor.safeRunForDist(() -> () -> Minecraft.getInstance().level, () -> () -> null))));
     public static final RegistryObject<ContainerType<ResearchTableContainer>> RESEARCH_TABLE_CONTAINER = REGISTER.register("research_table_container",
             () -> IForgeContainerType.create(
-                    (windowId, inv, data) -> new ResearchTableContainer(windowId, inv, data.readBlockPos(), DistExecutor.safeRunForDist(() -> () -> Minecraft.getInstance().level, () -> () -> null), PlayerKnowledges.deserializeNBT(data.readAnySizeNbt()))));
+                    (windowId, inv, data) -> new ResearchTableContainer(windowId, inv, data.readBlockPos(), inv.player.getCommandSenderWorld(), PlayerKnowledges.deserializeNBT(data.readAnySizeNbt()))));
     public static final RegistryObject<ContainerType<AssemblyTableContainer>> ASSEMBLY_TABLE_CONTAINER = REGISTER.register("magic_model_assembly_table_container",
             () -> IForgeContainerType.create(
-                    (windowId, inv, data) -> new AssemblyTableContainer(windowId, inv, data.readBlockPos(), DistExecutor.safeRunForDist(() -> () -> Minecraft.getInstance().level, () -> () -> null))));
+                    (windowId, inv, data) -> new AssemblyTableContainer(windowId, inv, data.readBlockPos(), inv.player.getCommandSenderWorld())));
     public static final RegistryObject<ContainerType<EngraverTableContainer>> ENGRAVER_TABLE_CONTAINER = REGISTER.register("engraver_table_container",
             () -> IForgeContainerType.create(
-                    (windowId, inv, data) -> new EngraverTableContainer(windowId, inv, data.readBlockPos(), DistExecutor.safeRunForDist(() -> () -> Minecraft.getInstance().level, () -> () -> null))));
+                    (windowId, inv, data) -> new EngraverTableContainer(windowId, inv, data.readBlockPos(), inv.player.getCommandSenderWorld())));
 }
